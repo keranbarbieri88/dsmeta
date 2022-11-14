371049f642ceb9a987bd1f34d691aff5fe3b9ede
Keran Barbieri <keranbarbieri@hotmail.com>
2022-11-14 18:50:04 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    //é necessário colocar as tags entre <></> (fragment) porque o React exige assim.
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+    </>
+
+
 
   )
 }
